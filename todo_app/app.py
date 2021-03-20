b9b361f074db45c8c1d84d0121e3c61e66ce4229
jordi <sgipower@gmail.com>
2021-03-20 11:21:59 +0000

    managed to follow Jack's advise and improved the code before starting exercise 2.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from todo_app.data import session_items
-from flask import Flask, render_template
-from flask.globals import request, session
+from flask import Flask, render_template,redirect,url_for
+from flask.globals import request
 
 from todo_app.flask_config import Config
 
@@ -13,34 +13,34 @@ def index_get():
     items =  session_items.get_items()
     return render_template('index.html',items=items)
 
+@app.route('/delete', methods=["POST"])
+def delete_item():
+    val = request.form.get("deletebutton")
+    session_items.delete_item(val)
+    return redirect(url_for('index_get'))
 
-@app.route('/', methods=["POST"])
-def edit_item():
-    if "deletebutton" in request.form:
-        val = request.form.get("deletebutton")
-        print(val)
-        session_items.delete_item(val)
-    elif "Complete" in request.form:
-        val = request.form.get("Complete")
-        item = session_items.get_item(int(val))
-        item['status'] = "Completed"
-        session_items.save_item(item)
-    elif "Reset" in request.form:
-        val = request.form.get("Reset")
-        item = session_items.get_item(int(val))
-        item['status'] = "Not Started"
-        session_items.save_item(item)
-    items =  session_items.get_items()
-    return render_template('index.html',items=items)
+@app.route('/reset', methods=["POST"])
+def reset_item():
+    val = request.form.get("Reset")
+    item = session_items.get_item(int(val))
+    item['status'] = "Not Started"
+    session_items.save_item(item)
+    return redirect(url_for('index_get'))
+
+@app.route('/complete', methods=["POST"])
+def complete_item():
+    val = request.form.get("Complete")
+    item = session_items.get_item(int(val))
+    item['status'] = "Completed"
+    session_items.save_item(item)
+    return redirect(url_for('index_get'))
 
 
 @app.route('/additem', methods=["POST"])
 def add_item():
     val = request.form.get("data")
-    print(val)
     session_items.add_item(val)
-    items =  session_items.get_items()
-    return render_template('index.html',items=items)
+    return redirect(url_for('index_get'))
 
 if __name__ == '__main__':
     app.run()
