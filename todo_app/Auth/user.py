507c7507e4a65d933bfc65bec468af4a248f8f8b
jordi <sgipower@gmail.com>
2021-11-20 13:20:38 +0000

    +

--- todo_app/Auth/user.py
+++ todo_app/Auth/user.py
@@ -3,7 +3,7 @@ from flask_login import UserMixin
 writers = [24620559]
 
 class User(UserMixin):
-    def __init__(self, id_=123, name = "name", email= "email", profile_pic= "profile_pic"):
+    def __init__(self, id_=123, name = "name", email= "email", profile_pic= "https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"):
         self.id = id_
         self.name = name
         self.email = email
