dacd735ffda3d4190f40fc512086479a35838091
jordi <sgipower@gmail.com>
2022-01-11 10:43:02 +0000

    added test file and tweaked flask_config file.

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -3,9 +3,6 @@ import os
 
 class Config:
     """Base configuration variables."""
-    MONGO_CONNECTION_STRING = os.environ.get('MONGO_CONNECTION_STRING')
-    if not MONGO_CONNECTION_STRING:
-       raise ValueError("No MONGO_CONNECTION_STRING set for Flask application. Did you follow the setup instructions?")
     SECRET_KEY = os.environ.get('SECRET_KEY')
     if not SECRET_KEY:
        raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
