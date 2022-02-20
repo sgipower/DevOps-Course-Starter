0a4de68a93d6b453c7a673de29bafbc9c40ae98b
jordi <sgipower@gmail.com>
2022-02-20 14:01:33 +0000

    improved outputs so no webhook is shown. added script so i don't have problems with special characters.

---
+++ terraform_deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+terraform init
+terraform apply -var="APP-PREFIX=$PRODUCTION_ENV_PREFIX" -var="CLIENTID=$CLIENT_ID" -var="CLIENTSECRET=$CLIENTSECRET"  -auto-approve
+curl -dH -X POST "$(terraform output -raw cd_webhook)"
