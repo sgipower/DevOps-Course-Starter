eec47cc16053a822c7951896eb57acc82b2bc224
sgipower <sgipower@gmail.com>
2022-02-22 15:15:31 +0000

    Module 12 (#15)
    merge

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "APP-PREFIX" {
+    type        = string
+    sensitive   = false
+}
+variable "location" {
+    description = "The Azure location where all resources in this deployment should be created"
+    default = "uksouth"
+}
+
+variable "CLIENTID" {
+    type        = string
+    sensitive   = true
+}
+variable "CLIENTSECRET" {
+    type        = string
+    sensitive   = true
+}
