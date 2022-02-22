a74186e0e99d552d1e2c923b5130384baa43f70d
sgipower <sgipower@gmail.com>
2022-02-22 14:57:53 +0000

    Update terraform_deploy.sh
    Co-authored-by: Ben Ramchandani <35066342+BenRamchandani@users.noreply.github.com>

--- terraform_deploy.sh
+++ terraform_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 terraform init
 terraform apply -var="APP-PREFIX=$PRODUCTION_ENV_PREFIX" -var="CLIENTID=$CLIENT_ID" -var="CLIENTSECRET=$CLIENTSECRET"  -auto-approve
-curl -dH -X POST "$(terraform output -raw cd_webhook)"
+curl -dH -X POST --fail "$(terraform output -raw cd_webhook)"
